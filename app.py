818ce17a9e94613bbbc613a48ee7cf6468a9eb40
Alex Lu <alex@Alexs-MacBook-Pro-4.local>
2018-12-28 16:54:54 -0500

    j

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
+X = 'your mom gay'
+
+@app.route('/hello/<name>')
+def hello_world(name):
+    return render_template('temp.html', X=name)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Gilvor'
-
+def hello_j():
+    return render_template('temp.html', X=request.remote_addr)
 
