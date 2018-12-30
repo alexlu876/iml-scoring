b04fce45e16de85f134bc483cf64e6cd23ee081b
Gilvir Gill <ggill@stuy.edu>
2018-12-30 01:48:42 -0500

    woo

--- iml/__init__.py
+++ iml/__init__.py
@@ -4,22 +4,29 @@ from flask import request
 from flask_session import Session
 from flask_migrate import Migrate
 
-from iml.config import *
+from iml.config import DATABASE_TYPE, SQLITE_FILE_NAME
+from iml.config import SQLALCHEMY_TRACK_MODIFICATIONS, APP_SECRET_KEY, SESSION_TYPE
 from iml.database import db
 from iml.core.user.controllers import user
 
-## pathing
-TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+import os
+
+# pathing
+TEMPLATE_DIR = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'templates')
+
+STATIC_DIR = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'static')
 
 app = Flask(__name__,
-        template_folder = TEMPLATE_DIR,
-        static_folder = STATIC_DIR)
+            template_folder=TEMPLATE_DIR,
+            static_folder=STATIC_DIR)
 
 
-### Config
+# Config
 if DATABASE_TYPE == 'sqlite':
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(SQLITE_FILE_NAME)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(
+        SQLITE_FILE_NAME)
 else:
     pass
 app.config['SECRET_KEY'] = APP_SECRET_KEY
@@ -28,7 +35,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 app.config['SESSION_SQLALCHEMY'] = db
 
 
-## set up session and db session
+# set up session and db session
 session = Session()
 session.init_app(app)
 
@@ -38,19 +45,16 @@ with app.app_context():
     db.session.commit()
 
 
-
 # automatic app migration
 
 migrate = Migrate(app, db)
 
 ### TEMPORARY ROUTER CHECKER ###
+
+
 @app.route('/')
 def hello_j():
     return render_template('temp.html', X=request.remote_addr)
 
 
-
-
-### blueprints ###
-
-app.register_blueprint(user,url_prefix="")
+app.register_blueprint(user, url_prefix="")
--- iml/forms.py
+++ iml/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, validators
 
 
 # for wtforms
+
+class LoginForm(FlaskForm):
+    email = StringField("Email: ",
+                        [validators.DataRequired(),
+                         validators.Length(min=3,
+                                           max=25)])
+    password = PasswordField("Password: ",
+                             [validators.DataRequired(),
+                              validators.Length(min=3,
+                                                max=25)])
+    submit = SubmitField("Login")
