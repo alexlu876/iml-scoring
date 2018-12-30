2872b6334675368c3fa5f3f267a88af734e8b266
Alex Lu <alex@Alexs-MacBook-Pro-4.local>
2018-12-29 19:32:03 -0500

    added columns

--- iml/core/user/controllers.py
+++ iml/core/user/controllers.py
@@ -2,3 +2,7 @@ from flask import Blueprint, render_template, session
 from iml.models import User
 
 user = Blueprint("user", __name__)
+
+@user.route("/login")
+def login():
+    return render_template("temp.html")
