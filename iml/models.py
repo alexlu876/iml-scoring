e6910126624f0098171d91e4a8ac62e048b1f970
Gilvir Gill <ggill@stuy.edu>
2018-12-30 01:27:14 -0500

    stfu

--- iml/models.py
+++ iml/models.py
@@ -1,28 +1,27 @@
 from iml.database import db
+import bcrypt
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,
-            primary_key=True)
+                   primary_key=True)
     first = db.Column(db.String(32), nullable=False)
     last = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), nullable=False)
     phone = db.Column(db.String(32), nullable=False)
-    #school = ??
+    # school = ??
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.Binary(60), nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
 
     def setPassword(self, newpass):
-        self.password = bcrypt.hashpw(newpass.encode("utf-8"), bcrypt.gensalt(12))
+        self.password = bcrypt.hashpw(newpass.encode("utf-8"),
+                                      bcrypt.gensalt(12))
 
     def checkPassword(self, password):
         return bcrypt.checkpw(password.encode("utf-8"), self.password)
-
-
-
 
 
 class Student(db.Model):
@@ -35,19 +34,15 @@ class Student(db.Model):
     pubid = db.Column(db.String(32), nullable=False)
 
 
-
-
 class Score(db.Model):
-    __tablename__  = 'scores'
+    __tablename__ = 'scores'
 
     id = db.Column(db.Integer, primary_key=True)
     contest_id = db.Column(db.Integer, nullable=False)
     student_id = db.Column(db.Integer, nullable=False)
-    coach_id  = db.Column(db.Integer, nullable=False)
+    coach_id = db.Column(db.Integer, nullable=False)
     team_id = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime(), nullable=False)
-
-
 
 
 class Contest(db.Model):
