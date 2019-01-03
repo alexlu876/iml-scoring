6fdf9751903b92ca102bcaac34907dfe9b9463fd
Gillgamesh <ggill@stuy.edu>
2019-01-02 20:29:27 -0500

    stuff5

--- iml/forms.py
+++ iml/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import validators
 
 
 # for wtforms
@@ -8,21 +9,31 @@ class LoginForm(FlaskForm):
     email = StringField("Email: ",
                         [validators.DataRequired(),
                          validators.Length(min=3,
-                                           max=25)])
+                                           max=25),
+                         validators.Email()]
+                       )
     password = PasswordField("Password: ",
                              [validators.DataRequired(),
                               validators.Length(min=3,
                                                 max=25)])
     submit = SubmitField("Login")
-	
-	
+
+
 class RegisterForm(FlaskForm):
-	email = StringField("Email: ",
-                        [validators.DataRequired(),
-                         validators.Length(min=3,
-                                           max=25)])
-	password = PasswordField("Password: ",
+    first = StringField("First Name:", [validators.DataRequired()])
+    last = StringField("Last Name:", [validators.DataRequired()])
+    password = PasswordField("Password: ",
                              [validators.DataRequired(),
                               validators.Length(min=3,
                                                 max=25)])
-	submit = SubmitField("Register")
+    phone_num = StringField("Phone Number:")
+
+    email = StringField("Email: ",
+                        [validators.DataRequired(),
+                         validators.Length(min=3,
+                                           max=25),
+                         validators.Email()],)
+
+    new_school = StringField("School (If not in Dropdown)")
+    old_school = SelectField("School: ", choices= [('Stuyvesant'), ('Stuyvesant')]
+    submit = SubmitField("Register")
--- iml/models.py
+++ iml/models.py
@@ -11,10 +11,14 @@ class User(db.Model):
     last = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), nullable=False)
     phone_num = db.Column(db.String(32), nullable=False)
-    # school = ??
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.Binary(60), nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False)
+    approval_status = db.Column(db.Integer)
+    school_id = db.Column(db.Boolean, db.ForeignKey('schools.id'))
+
+    school = db.relationship('School', back_populates='coaches')
+    scores = db.relationship('Score', back_populates='coach')
 
     def __init__(self, first, last, email, phone_num, username, password, isAdmin):
         self.first = first
@@ -41,19 +45,47 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.String(32), nullable=False)
     last = db.Column(db.String(32), nullable=False)
-    division = db.Column(db.String(32), nullable=False)
-    pubid = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(32), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'),nullable=False)
+
+    school = db.relationship('School', back_populates='students')
+    scores = db.relationship('Score', back_populates='student')
+
+
+    # returns a list of ({0,1}) integers with information on whether they got the questions right
+    def getScores(self, contest=None):
+        if contest is None:
+            scores_query = Score.query.filter_by(is_correct=1)
+        else:
+            scores_query = Score.query.filter_by(is_correct=1,contest=contest)
+        # TODO -  turn this into the list, because its an unordered query list
+        # sort by -- date of contest, question number
+
+    # returns actual final score
+    def getFinalContestScore(self, contest):
+        return sum(self.getScores(contest))
+    def getFinalScore(self):
+        return sum(self.getScores())
 
 
 class Score(db.Model):
     __tablename__ = 'scores'
-
     id = db.Column(db.Integer, primary_key=True)
-    contest_id = db.Column(db.Integer, nullable=False)
-    student_id = db.Column(db.Integer, nullable=False)
-    coach_id = db.Column(db.Integer, nullable=False)
-    team_id = db.Column(db.Integer, nullable=False)
+    # ASSERT: {0,1}
+    question_num = db.Column(db.Integer, nullable=False)
+    is_correct = db.Column(db.Integer, nullable=False)
+    contest_id = db.Column(db.Integer,db.ForeignKey('contests.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    coach_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     timestamp = db.Column(db.DateTime(), nullable=False)
+
+    # relationships
+
+    contest = db.relationship('Contest', back_populates = 'scores')
+    student = db.relationship('Student', back_populates = 'scores')
+    coach = db.relationship('User', back_populates = 'scores')
+    team = db.relationship('Team', back_populates = 'scores')
 
 
 class Contest(db.Model):
@@ -63,3 +95,53 @@ class Contest(db.Model):
     name = db.Column(db.String(32), nullable=False)
     date = db.Column(db.Date(), nullable=False)
     question_count = db.Column(db.Integer, nullable=False)
+    division_id = db.Column(db.Integer, db.ForeignKey('divisions.id'), nullable=False)
+
+    division = db.relationship('Division', back_populates='contests')
+
+    scores = db.relationship('Score', back_populates='contest')
+
+    def getScores(self):
+        return self.scores
+
+    # returns a list of students who have scores for a contest
+    def getAttendees(self):
+        pass
+
+
+class School(db.Model):
+    __tablename__ = 'schools'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+
+    # teams, schools, students, coaches
+    teams = db.relationship('Team', back_populates='school')
+    coaches = db.relationship('User', back_populates='school')
+    students = db.relationship('Student', back_populates='school')
+
+
+class Team(db.Model):
+    __tablename__ = 'teams'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'), nullable=False)
+    division_id = db.Column(db.Integer, db.ForeignKey('divisions.id'),
+                            nullable=False)
+
+    school = db.relationship('School', back_populates = 'teams')
+    division = db.relationship('Division', back_populates = 'teams')
+
+    scores = db.relationship('Score', back_populates = 'team')
+
+
+
+
+class Division(db.Model):
+    __tablename__ = 'divisions'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+
+    teams = db.relationship('Team', back_populates = 'division')
+    contests = db.relationship('Contest', back_populates = 'division')
