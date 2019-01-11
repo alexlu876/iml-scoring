3b657815adcb82a21571e77bc40e3cd734406538
Gillgamesh <ggill@stuy.edu>
2019-01-10 23:09:44 -0500

    updated colors, flashes

--- iml/static/js/mdc-nav.js
+++ iml/static/js/mdc-nav.js
@@ -1,6 +1,8 @@
 const MDCDrawer = mdc.drawer.MDCDrawer;
 const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar;
 const MDCTextField = mdc.textField.MDCTextField;
+const MDCNotchedOutline = mdc.notchedOutline.MDCNotchedOutline;
+const MDCSnackbar = mdc.snackbar.MDCSnackbar;
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
@@ -9,12 +11,9 @@ const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 
 const textFields = [].map.call(document.querySelectorAll('.mdc-text-field'), function(e) {
   return new MDCTextField(e);
-
-
 });
-// topAppBar.listen('MDCTopAppBar:nav', () => {
-//     drawer.open = !drawer.open;
-// });
+
+//topbar and drawer
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
 });
@@ -34,3 +33,21 @@ $(".drawer-haschild").on("click",function(){
 });
 
 
+
+// snackbar for flashes
+// VERY TEMPORARY SOLUTION, MUST BE REWORKED
+const flashes = [].map.call(document.querySelectorAll('.mdc-snackbar'), function(e) {
+    return new MDCSnackbar(e);
+});
+
+for (i in flashes) {
+    flashes[i].open();
+    flashes[i].timeoutMs = 5000+50*i;
+}
+
+// const notchedOutlines = [].map.call(document.querySelectorAll('.mdc-notched-outline'), function(e) {
+//     return new MDCNotchedOutline(e);
+// });
+// topAppBar.listen('MDCTopAppBar:nav', () => {
+//     drawer.open = !drawer.open;
+// });
