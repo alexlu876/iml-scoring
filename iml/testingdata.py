1c44835f6886b3646bcdef439301dc064b081586
Alex Lu <alex@Alexs-MBP-4.fios-router.home>
2019-01-14 16:34:47 -0500

    j

--- iml/testingdata.py
+++ iml/testingdata.py
@@ -5,16 +5,21 @@ import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.tools as tls
+dict = {}
+dict[0] = 2
+dict[1] = 5
+dict[2] = 8
+dict[3] = 11
+dict[4] = 12
+dict[5] = 8
+dict[6] = 4
+commutes = pd.Series(dict)
 
-size = 1000
-scale = 10
-commutes = pd.Series(np.random.gamma(scale, size=size) ** 1.5)
-
-commutes.plot.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+commutes.plot.hist(grid=True, color='#607c8e')
 plt.title = 'Commute time'
 plt.xlabel = 'time'
 plt.ylabel = 'freq'
-plt.grid(axis='y', alpha=0.75)
+plt.grid(axis='y', alpha=0.25)
 
 fig = plt.gcf()
 plotly_fig = tls.mpl_to_plotly(fig)
