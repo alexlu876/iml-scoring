275a32586062bbd2c105594f19cf978ed7782eb6
Gilvir Gill <ggill@stuy.edu>
2019-01-14 15:29:42 -0500

    slight change to contet

--- iml/static/js/core/admin/add_contest.js
+++ iml/static/js/core/admin/add_contest.js
@@ -1,7 +1,7 @@
 const fp_contest = flatpickr("#start_time", {
     altInput: true,
-    altFormat: "F j, Y h:iK",
-    enableTime: true,
+    altFormat: "F j, Y",
+    enableTime: false,
     dateFormat: 'Y-m-d H:i',
     minDate: "today",
 });
