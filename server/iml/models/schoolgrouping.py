41c69a2f463ae30c5522b5a96071333fa7dd81d7
Andrew <andrewxinchen@gmail.com>
2019-06-13 16:11:13 -0400

    schoolgrouping relationship

--- server/iml/models/schoolgrouping.py
+++ server/iml/models/schoolgrouping.py
@@ -1,12 +1,12 @@
 from iml import db
 
-class Schoolgrouping(db.Model):
+class SchoolGrouping(db.Model):
 
-    __tablename__ = 'schoolgroupings'
+    __tablename__ = 'school_groupings'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
 
+    schools = db.relationship('schools', back_populates='school_groupings')
+
     def __init__(self, name):
         self.name = name
-
-
