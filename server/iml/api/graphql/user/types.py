49a0952beef8aea09020faa3cca2ad43cb87e33a
gillgamesh <gilvirgillgg@gmail.com>
2019-07-23 03:14:16 -0400

    sql changes

--- server/iml/api/graphql/user/types.py
+++ server/iml/api/graphql/user/types.py
@@ -1,7 +1,14 @@
 from iml.models import User as UserModel
+
+import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 class User(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
         exclude_fields=("password",)
+        interfaces = (graphene.relay.Node,)
+
+class UserRelayConnection(graphene.relay.Connection):
+    class Meta:
+        node = User
